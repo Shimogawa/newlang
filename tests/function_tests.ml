667974767d5a2eec2c58ded6bd92a93d046aec8c
Rebuild <admin@rebuild.moe>
2022-02-08 23:26:49 -0500

    add more tests

--- tests/function_tests.ml
+++ tests/function_tests.ml
@@ -19,9 +19,75 @@ let test_fun_with_args () =
        {|f = function(x, y, z) { return x + y + z; }; f("a", "b", "c");|};
      States.newlang_state#last_expr_val)
 
+let test_fun_closure () =
+  Alcotest.(check Common.val_t)
+    "closure 1" (String "a")
+    (Common.execute
+       {|f = function() { return function() { return "a"; }; }; f()();|};
+     States.newlang_state#last_expr_val);
+  Alcotest.(check Common.val_t)
+    "closure 2" (Int 2)
+    (Common.execute
+       {|
+       f = function() { a = 0; return function() { a = a + 1; return a; }; };
+       c = f();
+       c();
+       c();
+       |};
+     States.newlang_state#last_expr_val);
+  Alcotest.(check Common.val_t)
+    "closure 3" (Int 4)
+    (Common.execute
+       {|
+        a = 2;
+        f = function() { return function() { a = a + 1; return a; }; };
+        c = f();
+        c();
+        c();
+        |};
+     States.newlang_state#last_expr_val);
+  Alcotest.(check Common.val_t)
+    "closure 4" (Int 6)
+    (Common.execute
+       {|
+        f = function() {
+          a = 2;
+          return function() {
+            b = 0;
+            return function () {
+              a = a + 1;
+              b = b + 1;
+              return a + b;
+            };
+          };
+        };
+        c = f()();
+        c();
+        c();
+        |};
+     States.newlang_state#last_expr_val);
+  Alcotest.(check Common.val_t)
+    "closure 5" (Int 5)
+    (Common.execute
+       {|
+        delay = function(f) {
+          result = null;
+          return function() {
+            if (result == null) {
+              result = f();
+            };
+            return result;
+          };
+        };
+        d = delay(function () { return 5; });
+        d();
+        |};
+     States.newlang_state#last_expr_val)
+
 let test_cases =
   [
     Alcotest.test_case "simple function" `Quick test_simple_fun;
     Alcotest.test_case "function with return" `Quick test_fun_with_ret;
     Alcotest.test_case "function with args" `Quick test_fun_with_args;
+    Alcotest.test_case "closure" `Quick test_fun_closure;
   ]
--- tests/val_tests.ml
+++ tests/val_tests.ml
@@ -6,6 +6,12 @@ let test_int () =
     (Common.execute {|3;|};
      States.newlang_state#last_expr_val)
 
+let test_float () =
+  Alcotest.(check Common.val_t)
+    "same val" (Float 7.0)
+    (Common.execute {|7.0;|};
+     States.newlang_state#last_expr_val)
+
 let test_add () =
   Alcotest.(check Common.val_t)
     "int add" (Int 7)
@@ -24,16 +30,17 @@ let test_add () =
     (Common.execute {|3 + "4";|};
      States.newlang_state#last_expr_val);
   Alcotest.(check Common.val_t)
-    "string add 3" (Float 7.0)
+    "float add 1" (Float 7.0)
     (Common.execute {|3 + 4.0;|};
      States.newlang_state#last_expr_val);
   Alcotest.(check Common.val_t)
-    "string add 3" (Float 7.0)
+    "float add 2" (Float 7.0)
     (Common.execute {|3.0 + 4;|};
      States.newlang_state#last_expr_val)
 
 let test_cases =
   [
     Alcotest.test_case "integer" `Quick test_int;
+    Alcotest.test_case "float" `Quick test_float;
     Alcotest.test_case "add" `Quick test_add;
   ]
